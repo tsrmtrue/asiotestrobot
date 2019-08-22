7021a27ff8e393ab7a2d1a39cd5625a55ba42b63
haytang <haytang@123u.com>
2019-08-22 16:13:24 +0800

    正确的消息队列

--- AESever/common/ae_lock.h
+++ AESever/common/ae_lock.h
@@ -5,33 +5,29 @@
 
 struct EasySpinLock
 {
-    #define LOCK_VALUE 1
-    #define UNLOCK_VALUE 0
-    std::atomic_char _lock{UNLOCK_VALUE};
+    std::atomic_flag _lock{ ATOMIC_FLAG_INIT };
 
-    //◊Ë»˚µ»
-    void Lock()
-    {
-        char _target = UNLOCK_VALUE;
-        while(!_lock.compare_exchange_weak(_target, LOCK_VALUE, std::memory_order_acquire))
-        {
-            _target = UNLOCK_VALUE;
-        }
-    }
+    //ÈòªÂ°ûÁ≠â
+    //void Lock()
+    //{
+    //    char _target = UNLOCK_VALUE;
+    //    while(!_lock.compare_exchange_weak(_target, LOCK_VALUE, std::memory_order_acquire))
+    //    {
+    //        _target = UNLOCK_VALUE;
+    //    }
+    //}
 
-    //≤ª◊Ë»˚µ»
+    //‰∏çÈòªÂ°ûÁ≠â
     bool TryLock()
     {
-        char _target = UNLOCK_VALUE;
-        return _lock.compare_exchange_weak(_target, LOCK_VALUE, std::memory_order_acquire);
-
+        bool r = _lock.test_and_set(std::memory_order_acquire);
+        return r == false;
     }
 
-    //Ω‚À¯
+    //Ëß£ÈîÅ
     void UnLock()
     {
-        _lock.store(UNLOCK_VALUE , std::memory_order_release);
+        _lock.clear(std::memory_order_release);
     }
 };
-
 #endif
