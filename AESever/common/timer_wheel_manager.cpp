3b91e00feb9f792d50edb3bf7e903e98f1043e53
haytang@123u.com <haytang@123u.com>
2019-12-09 21:35:00 +0800

    一些代码

--- AESever/common/timer_wheel_manager.cpp
+++ AESever/common/timer_wheel_manager.cpp
@@ -2,6 +2,26 @@
 
 INSTANCE_SINGLETON(TimerWheelManager);
 
+bool TimerObj::WillTriggerNextTime()
+{
+    if (count == -1)
+    {
+        return true;
+    }
+    if (count == 0 || cd == 0)
+    {
+        return false;
+    }
+    count--;
+    //如果设置count 1 那么就触发一次
+    if (count == 0)
+    {
+        return false;
+    }
+    //处理cd
+    next_triggle_time += cd;
+    return true;
+}
 
 
 bool TimerWheelManager::Init()
@@ -17,5 +37,52 @@ bool TimerWheelManager::Init()
 };
 void TimerWheelManager::Uninit()
 {
-    
+ 
 };
+
+TIMER_ID TimerWheelManager::AddTimer(TimerObj*to)
+{
+    if (  !to 
+        || to->cd == 0 
+        || to->count == 0)
+    {
+        return INVALID_TIMER_ID;
+    }
+    //
+    if (to->next_triggle_time == 0)
+    {
+        to->next_triggle_time = now_time_ + to->cd;
+    }
+    uint64_t trigger_time = to->next_triggle_time > now_time_ ? to->next_triggle_time - now_time_: 0;
+    uint32_t idx1 = trigger_time % (WHEEL_LENGTH);
+    uint32_t idx2 = (trigger_time>> WHEEL_BITS) % (WHEEL_LENGTH);
+    uint32_t idx3 = ((trigger_time >> WHEEL_BITS) >> WHEEL_BITS) % (WHEEL_LENGTH);
+    uint32_t idx4 = (((trigger_time >> WHEEL_BITS) >> WHEEL_BITS) >> WHEEL_BITS);
+    if (idx4 > 0)
+    {
+        outer_list_.push_back(to);
+    }
+    else if (idx3 > 0)
+    {
+        idx3 = (idx3 + timer_index_[WHEEL_INDEX_3])% WHEEL_LENGTH;
+        total_timer_[WHEEL_INDEX_3][idx3].emplace_back(to);
+    }
+    else if (idx2 > 0)
+    {
+        idx2 = (idx2 + timer_index_[WHEEL_INDEX_2]) % WHEEL_LENGTH;
+        total_timer_[WHEEL_INDEX_2][idx2].emplace_back(to);
+    }
+    else 
+    {
+        idx1 = (idx1 + timer_index_[WHEEL_INDEX_1]) % WHEEL_LENGTH;
+        total_timer_[WHEEL_INDEX_1][idx1].emplace_back(to);
+    }
+    return to->id;
+ 
+}
+
+void TimerWheelManager::KillTimer(TIMER_ID handle)
+{
+    
+}
+
--- AESever/common/timer_wheel_manager.h
+++ AESever/common/timer_wheel_manager.h
@@ -16,6 +16,7 @@
      */
 
 #define  TIMER_ID uint64_t
+#define  INVALID_TIMER_ID 0
 
 using TimerCB = std::function<void()>;
 
@@ -38,14 +39,20 @@ struct TimerObj
     int32_t count{ 0 };
     //回调
     TimerCB cb{nullptr};
+    bool WillTriggerNextTime();
 };
 
-#define WHEEL_LENGTH 1024
-#define WHEEL_COUNT 3
+#define WHEEL_BITS 10
+#define WHEEL_LENGTH (2<<WHEEL_BITS)
+#define WHEEL_INDEX_1 0
+#define WHEEL_INDEX_2 (WHEEL_INDEX_1 + 1)
+#define WHEEL_INDEX_3 (WHEEL_INDEX_2 + 1)
+#define WHEEL_COUNT_MAX (WHEEL_INDEX_3 + 1 )
 
 using TimerObjList = std::list<TimerObj*>;
 using TimerObjListWheel = std::array<TimerObjList, WHEEL_LENGTH>;
-using TotalTimer = std::array<TimerObjListWheel, WHEEL_COUNT>;
+using TotalTimer = std::array<TimerObjListWheel, WHEEL_COUNT_MAX>;
+using TimerIndex = std::array<uint32_t, WHEEL_COUNT_MAX>;
 
 class TimerWheelManager
 {
@@ -55,17 +62,25 @@ public:
     DECLARE_SINGLETON(TimerWheelManager);
 
 public:
-    TIMER_ID AddTimer() { return 0; };
-    void KillTimer(TIMER_ID handle){};
+
+    TIMER_ID AddTimer(TimerObj*);
+    void KillTimer(TIMER_ID handle);
     bool Init();
     void Uninit();
 
 private:
+    /*
+     *去到当前的时间片，从容器中全部取出，遍历执行回调，如果需要下次回调则插入下一个空洞，
+     *问题1，timer每毫秒都必须跳动。
+     *问题2，删除的时候需要快速定位。
+     */
     void Update(uint64_t time_stampe){};
 
 private:
     uint64_t now_time_{ 0 };
     TotalTimer total_timer_;
+    TimerIndex timer_index_{0,0,0};
+    TimerObjList outer_list_;//超长时间的timer，当所有timer队列都遍历过一遍，则这里刷新一次。
    
 
 };
